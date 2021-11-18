83870fe660152ce28eb6eed93b71e9f4aa654ef2
Layla-p <layla.porter@gmail.com>
2021-11-18 16:46:42 +0000

    added licence and readme

--- ThemeDemo/Program.cs
+++ ThemeDemo/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using ThemeDemo;
+using BlazorThemeSwitchDemo;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
